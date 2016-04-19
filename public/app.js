a9920749334552106ed000853e9c8553cb689b49
Justin Nguyen <jnguyen3950@gmail.com>
2016-04-19 09:55:51 -0700

    Angular route exercise.

---
+++ public/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('todo', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/todo', {
+    templateUrl: 'todo/show.directive.html',
+    controller: 'todoController',
+    controllerAs: 'todo'
+  })
+}]);
